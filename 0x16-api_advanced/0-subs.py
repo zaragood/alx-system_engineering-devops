2e94ec2cc7fa017a3444d9a45c1984090963951c
zaragood <azaragoodness52@gmail.com>
2024-01-16 12:44:48 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,11 @@
 """  function that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """If not a valid subreddit, return 0 """
     url = "https://www.reddit.com//r/{}/about.json".format(subreddit)
     headers = {"User-agent": "0-sub/1.0"}
 
